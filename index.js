edc85260926d6710efe74cfaf133256f24eb3b3c
George Samuel Igoe <george.igoe@gmail.com>
2018-06-10 19:37:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+function Arrays() {
+  beforeEach(function() {
+    window.kittens = ["Milo", "Otis", "Garfield"];
+  })
+}
+
+function kittens () {
+  var kittens = ["Milo", "Otis", "Garfield"];
+}
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  [...kittens, name];
+  return kittens;
+}
+
+function prependKitten(name) {
+  [name, ...kittens];
+  return kittens;
+}
